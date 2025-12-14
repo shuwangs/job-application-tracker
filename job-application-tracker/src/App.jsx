dfa4d9fe3f4e1d3a58c5ae8ae74fd38f904d7f0d
Paige Li <itspaige.nli@gmail.com>
2025-12-13 22:49:36 -0800

    Update table css

--- job-application-tracker/src/App.jsx
+++ job-application-tracker/src/App.jsx
@@ -1,15 +1,12 @@
 import "./App.css";
 import { Table } from "./components/Table";
-// Import BrowserRouter
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Table />
-      </div>
-    </Router>
+    <div className="App">
+      <Table />
+    </div>
   );
 }
+
 export default App;
--- job-application-tracker/src/components/Table.jsx
+++ job-application-tracker/src/components/Table.jsx
@@ -1,19 +1,48 @@
 import React from "react";
+import { BsFillTrashFill, BsFillPencilFill } from "react-icons/bs";
+
 import "./Table.css";
 
 export const Table = () => {
   return (
-    <div>
-      <table>
+    <div className="table-wrapper">
+      <table className="table">
         <thead>
           <tr>
-            <th>Company</th>
-            <th>Job Title</th>
-            <th>Status</th>
-            <th>Actions</th>
+            <th className="company">Company</th>
+            <th className="job-title">Job Title</th>
+            <th className="status">Status</th>
+            <th className="actions">Actions</th>
           </tr>
         </thead>
-        <tbody></tbody>
+        <tbody>
+          <tr>
+            <td className="input1">e.g. Playstation</td>
+            <td className="input2">e.g. Software Engineer</td>
+            <td>
+              <span className="input3">e.g. Pending</span>
+            </td>
+            <td>
+              <span className="actions">
+                <BsFillTrashFill className="delete-btn" />
+                <BsFillPencilFill />
+              </span>
+            </td>
+          </tr>
+          <tr>
+            <td className="input1">e.g. Playstation</td>
+            <td className="input2">e.g. Software Engineer</td>
+            <td>
+              <span className="input3">e.g. Pending</span>
+            </td>
+            <td>
+              <span className="actions">
+                <BsFillTrashFill className="delete-btn" />
+                <BsFillPencilFill />
+              </span>
+            </td>
+          </tr>
+        </tbody>
       </table>
     </div>
   );
