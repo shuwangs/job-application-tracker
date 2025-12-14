bac2917c217f4b4c6f663a83765d2a05cd5a7936
Paige Li <itspaige.nli@gmail.com>
2025-12-14 08:26:00 -0800

    Adding Violetta's Changes for form to my table and my form events

--- job-application-tracker/src/components/Dashboard.jsx
+++ job-application-tracker/src/components/Dashboard.jsx
@@ -1,17 +1,67 @@
 import { useState } from "react";
-import React from "react";
 import { Table } from "./Table";
 import { Modal } from "./Modal";
 
 export const Dashboard = () => {
   const [modalOpen, setModalOpen] = useState(false);
+  const [rows, setRows] = useState([
+    {
+      company: "Microsoft",
+      position: "Frontend Engineer",
+      description: "Working on UI components.",
+      dateApplied: "2025-12-10",
+      status: "Applied",
+    },
+    {
+      company: "Nintendo",
+      position: "Game Developer",
+      description: "Gameplay and UI work.",
+      dateApplied: "2025-12-11",
+      status: "Interviewing",
+    },
+  ]);
+  const [rowToEdit, setRowToEdit] = useState(null);
+
+  const handleDeleteRow = (targetIndex) => {
+    setRows(rows.filter((_, idx) => idx !== targetIndex));
+  };
+
+  const handleEditRow = (idx) => {
+    setRowToEdit(idx);
+
+    setModalOpen(true);
+  };
+
+  const handleSubmit = (newRow) => {
+    rowToEdit === null
+      ? setRows([...rows, newRow])
+      : setRows(
+          rows.map((currRow, idx) => {
+            if (idx !== rowToEdit) return currRow;
+
+            return newRow;
+          })
+        );
+  };
+
   return (
     <div className="Dashboard">
-      <Table />
-      <button className="btn" onClick={() => setModalOpen(true)}>
-        Add New
+      <Table rows={rows} deleteRow={handleDeleteRow} editRow={handleEditRow} />
+      <button onClick={() => setModalOpen(true)} className="btn">
+        Add
       </button>
-      {modalOpen && <Modal />}
+      {modalOpen && (
+        <Modal
+          closeModal={() => {
+            setModalOpen(false);
+            setRowToEdit(null);
+          }}
+          onSubmit={handleSubmit}
+          defaultValue={rowToEdit !== null && rows[rowToEdit]}
+        />
+      )}
     </div>
   );
 };
+
+export default Dashboard;
