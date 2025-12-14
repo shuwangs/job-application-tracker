30933dba1be4c70ad08dc193983d5b330bda472f
Paige Li <itspaige.nli@gmail.com>
2025-12-13 23:10:13 -0800

    Create Modal container

--- job-application-tracker/src/App.jsx
+++ job-application-tracker/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import { Table } from "./components/Table";
+import { Modal } from "./components/Modal";
 
 function App() {
   return (
     <div className="App">
       <Table />
+      <Modal />
     </div>
   );
 }
---
+++ job-application-tracker/src/components/Modal.jsx
@@ -0,0 +1,13 @@
+import "./Modal.css";
+
+export const Modal = () => {
+  return (
+    <div className="modal-container">
+      <div className="modal">
+        <form>
+          <div>INPUT VIOLETTA'S FORM HERE?</div>
+        </form>
+      </div>
+    </div>
+  );
+};
