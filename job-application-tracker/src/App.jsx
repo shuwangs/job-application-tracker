e8c7e61f981c464ab338f1e203e585dd6a57752b
Paige Li <itspaige.nli@gmail.com>
2025-12-13 23:37:33 -0800

    Creating Dashboard for Table view

--- job-application-tracker/src/App.jsx
+++ job-application-tracker/src/App.jsx
@@ -1,8 +1,7 @@
 import "./App.css";
 import React from "react";
 import LogIn from "./components/LogIn.jsx";
-import { Table } from "./components/Table";
-import { Modal } from "./components/Modal";
+import { Dashboard } from "./components/Dashboard.jsx";
 import { Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
@@ -11,7 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<LogIn />} />
-        <Route path="/dashboard" element={<Table />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </div>
   );
