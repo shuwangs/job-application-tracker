07aca2590d330ac678d9d77deb23ce3b052e7850
Paige Li <itspaige.nli@gmail.com>
2025-12-14 00:35:45 -0800

    Create Modal for an input form

--- job-application-tracker/src/components/Dashboard.jsx
+++ job-application-tracker/src/components/Dashboard.jsx
@@ -1,12 +1,17 @@
+import { useState } from "react";
 import React from "react";
 import { Table } from "./Table";
 import { Modal } from "./Modal";
 
 export const Dashboard = () => {
+  const [modalOpen, setModalOpen] = useState(false);
   return (
     <div className="Dashboard">
       <Table />
-      <Modal />
+      <button className="btn" onClick={() => setModalOpen(true)}>
+        Add New
+      </button>
+      {modalOpen && <Modal />}
     </div>
   );
 };
