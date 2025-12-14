10186eda2256496df4f72efb53284b487bf851ba
Paige Li <itspaige.nli@gmail.com>
2025-12-14 00:35:45 -0800

    Create Modal for an input form

--- job-application-tracker/src/components/Modal.jsx
+++ job-application-tracker/src/components/Modal.jsx
@@ -5,7 +5,27 @@ export const Modal = () => {
     <div className="modal-container">
       <div className="modal">
         <form>
-          <div>INPUT VIOLETTA'S FORM HERE?</div>
+          <div className="form-group">
+            <label htmlFor="page">Company Name</label>
+            <textarea name="page" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="description">Job Title</label>
+            <textarea name="description" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="status">Status</label>
+            <select name="status">
+              <option value="pending">Pending</option>
+              <option value="approved">Approved</option>
+              <option value="rejected">Rejected</option>
+              <option value="first interview">First Interview</option>
+              <option value="second interview">Second Interview</option>
+            </select>
+          </div>
+          <button type="submit" className="btn">
+            Submit
+          </button>
         </form>
       </div>
     </div>
